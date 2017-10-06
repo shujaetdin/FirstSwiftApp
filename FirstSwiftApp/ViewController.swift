f11d471b78bdfb1784130d4244d433ccde0c30c4
shujaetdin <shujaetdin@gmail.com>
2017-10-06 09:22:58 -0600

    I created random color stuff

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -11,14 +11,38 @@ import UIKit
 class ViewController: UIViewController
 {
     @IBOutlet weak var firstButton :UIButton!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func firstMethod(_ sender: UIButton) {
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+   if (firstButton.backgroundColor == .orange)
+   {
+    firstButton.backgroundColor = .black
+   }
+        else
+   {
+    firstButton.backgroundColor = .orange
+   }
+    view.backgroundColor= createRandomColor()
     }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha:
+            CGFloat(1.0))
+        
+        return newColor
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
